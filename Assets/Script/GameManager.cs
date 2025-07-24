c5235cff54d1195c2b397ce182d9859f8e436783
Alex Rodriguez <alex.rodriguez6@g.austincc.edu>
2025-07-24 15:45:12 -0500

    Added summary comments to the script

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -10,19 +10,33 @@ public class GameManager : MonoBehaviour
     public UIManager uiManager;
     public BounceUI bounceUI;
     private int score = 0;
-
+    /// <summary>
+    /// Allows the GameManager to be a singleton instance.
+    /// Turns off the PipeSpawner component to prevent pipes from spawning at the start.
+    /// </summary>
     void Awake()
     {
         Instance = this;
         pipeSpawner.enabled = false;
     }
-
+    /// <summary>
+    /// Calls the ShowStart method from the UIManager to display the start screen.
+    /// Turns off the Bird game object to hide it at the start of the game.
+    /// </summary>
     void Start()
     {
         uiManager.ShowStart();
         bird.gameObject.SetActive(false);
     }
-
+    /// <summary>
+    /// When the game is reset, this method is called to reset the game state.
+    /// Finds all the pipes with the script attached to them
+    /// Collects them in an arry and destroys them.
+    /// Resets the score to 0 and updates the UI.
+    /// Resets the UI to bounce
+    /// Resets the bird and turns it off.
+    /// Sets timer to 1;
+    /// </summary>
     public void ResetGame()
     {
         Pipes[] leftOverObstcles = FindObjectsByType<Pipes>(FindObjectsSortMode.None);
@@ -40,7 +54,12 @@ public class GameManager : MonoBehaviour
         bird.gameObject.SetActive(false);
         Time.timeScale = 1f;
     }
-
+    /// <summary>
+    /// When thee game is ready to start, this method is called to set up the game state.
+    /// Hides the start screen and shows the ready screen.
+    /// Resets the bird to its initial state.
+    /// turrns on the bird game object to make it visible.
+    /// </summary>
     public void ReadyGame()
     {
         uiManager.HideStart();
@@ -48,7 +67,12 @@ public class GameManager : MonoBehaviour
         bird.ResetBird();
         bird.gameObject.SetActive(true);
     }
-
+    /// <summary>
+    /// Starts the game by resetting the score to 0
+    /// Hides the ready screen.
+    /// Tunrs on the PipeSpawner component to start spawning pipes.
+    /// Turns on the bird to make it visible.
+    /// </summary>
     public void StartGame()
     {
         score = 0;
@@ -56,13 +80,19 @@ public class GameManager : MonoBehaviour
         pipeSpawner.enabled = true;
         bird.StartGame();
     }
-
+    /// <summary>
+    /// Sets timer to 0 to stop the game.
+    /// Shows the game over screen using the UIManager.
+    /// </summary>
     public void GameOver()
     {
         Time.timeScale = 0f;
         uiManager.ShowGameOver();
     }
-
+    /// <summary>
+    /// Increases the score by 1 each time this method is called.
+    /// Sends the score to be converted and displayed in the UIManager.
+    /// </summary>
     public void IncreaseScore()
     {
         score++;
