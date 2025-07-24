8e3ce1d4abd0b47f5d6986f9e5863a0564881db3
Alex Rodriguez <alex.rodriguez6@g.austincc.edu>
2025-07-24 15:09:40 -0500

    Added summary notes and notes to the script cleaned it up a bit.

--- Assets/Script/PipeSpawner.cs
+++ Assets/Script/PipeSpawner.cs
@@ -6,9 +6,13 @@ public class PipeSpawner : MonoBehaviour
     public float spawnRate = .1f;
     public float randomVariable = 1f;
     public Transform spawnLocation;
-    
+    // sets timer to a high value to ensure the first spawn does not happen immediately until the first Update call
     private float timer = float.MaxValue;
-
+    /// <summary>
+    /// Sets timer to 0 and starts the timer at a cosistent rate.
+    /// Starts spawning pipes at a random height within the specified range.
+    /// Sets timer to 0 so that the spawning is spaced out.
+    /// </summary>
     void Update()
     {
         timer += Time.deltaTime;
@@ -18,12 +22,19 @@ public class PipeSpawner : MonoBehaviour
             timer = 0f;
         }
     }
-
+    /// <summary>
+    /// Turns on the PipeSpawner component to start spawning pipes.
+    /// Turns on the script.
+    /// </summary>
     public void StartSpawning()
     {
         enabled = true;
     }
-    
+    /// <summary>
+    /// Sets the a radonom heigth (y) offset for the pipe spawn location
+    /// Sets the spawn position to the spawn location with the random height offset.
+    /// spawns a pipe with the new hight opening at the spawn position.
+    /// </summary>
     void SpawnRandomOpening()
     {
         float yOffset = Random.Range(-randomVariable, randomVariable);
