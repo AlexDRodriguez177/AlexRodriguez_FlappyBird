2aa944a8f480248044e8b7dd755498f758827960
Alex Rodriguez <alex.rodriguez6@g.austincc.edu>
2025-07-24 00:15:32 -0500

    Add project files.

---
+++ Assets/Script/GroundScroll.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class GroundScroll : MonoBehaviour
+{
+    public float scrollSpeed = 2f;
+    public float resetPositionX; 
+    public float initalPositionX; 
+
+    void Update()
+    {
+        transform.Translate(Vector2.left * scrollSpeed * Time.deltaTime);
+
+        if (transform.position.x <= resetPositionX)
+        {
+            Vector2 newPos = new Vector2(initalPositionX, transform.position.y);
+            transform.position = newPos;
+        }
+    }
+}
