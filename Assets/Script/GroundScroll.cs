06bce54686a02e6fb8a19ef80d28a24727a562bf
Alex Rodriguez <alex.rodriguez6@g.austincc.edu>
2025-07-24 15:51:35 -0500

    Creates a Summary comment

--- Assets/Script/GroundScroll.cs
+++ Assets/Script/GroundScroll.cs
@@ -6,7 +6,13 @@ public class GroundScroll : MonoBehaviour
     public float scrollSpeed = 2f;
     public float resetPositionX; 
     public float initalPositionX;
-
+    /// <summary>
+    /// Moves the ground to the left at a specified speed
+    /// Keeps a cosistent speed for the ground to scroll
+    /// When the ground reaches a certain position on the x-axis, it resets its position
+    /// Sets the position of the ground to its initial position on the x-axis
+    /// Makes the new position the initial position of the ground
+    /// </summary>
     void Update()
     {
         transform.Translate(Vector2.left * scrollSpeed * Time.deltaTime);
