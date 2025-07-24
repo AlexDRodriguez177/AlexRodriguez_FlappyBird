f2d53cc7e820d16801c6b8f553ca71c827f37d12
Alex Rodriguez <alex.rodriguez6@g.austincc.edu>
2025-07-24 14:38:30 -0500

    Added summary comments to the pipe script describing what it does.

--- Assets/Script/Pipes.cs
+++ Assets/Script/Pipes.cs
@@ -5,7 +5,12 @@ public class Pipes : MonoBehaviour
     public float moveSpeed = 2f;
     public float destroyX = -10f;
     private bool scored = false;
-
+   
+    /// <summary>
+    /// Moves the pipes to the left by using the move speed and multiplying it by time.delttime for a cosistent frame rate
+    /// If the pipe position is less than 0 and has not been scored yet it will set score to true and increase the score
+    /// Once the object moves past the destroy float on the x axis then the game object will be destoryed automatically
+    /// </summary>
     void Update()
     {
         transform.position += Vector3.left * moveSpeed * Time.deltaTime;
